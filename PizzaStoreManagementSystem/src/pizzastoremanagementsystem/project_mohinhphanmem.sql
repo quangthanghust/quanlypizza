de433ecf6b7c3574f315893e620a2fdc0401b3ca
HungNguyen20207677 <Hung.NQ207677@sis.hust.edu.vn>
2022-07-07 17:19:48 +0700

    Updated project

--- PizzaStoreManagementSystem/src/pizzastoremanagementsystem/project_mohinhphanmem.sql
+++ PizzaStoreManagementSystem/src/pizzastoremanagementsystem/project_mohinhphanmem.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2022 at 06:48 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Generation Time: Jul 07, 2022 at 12:18 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,6 +44,29 @@ INSERT INTO `accounttable` (`ACCID`, `NAME`, `PASSWORD`, `Gender`) VALUES
 (3, 'Thang', '02468', 'Male'),
 (4, 'Long', '13579', 'Male'),
 (5, 'Luong', '54321', 'Male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soldproductstable`
+--
+
+CREATE TABLE `soldproductstable` (
+  `PROID` int(11) NOT NULL,
+  `NAME` varchar(15) NOT NULL,
+  `PRICE` int(11) NOT NULL,
+  `SOLDAMOUNT` int(11) NOT NULL,
+  `TOTAL` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `soldproductstable`
+--
+
+INSERT INTO `soldproductstable` (`PROID`, `NAME`, `PRICE`, `SOLDAMOUNT`, `TOTAL`) VALUES
+(1, 'pizza A', 100000, 2, 200000),
+(2, 'pizza B', 150000, 5, 750000),
+(3, 'pizza C', 180000, 3, 540000);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
